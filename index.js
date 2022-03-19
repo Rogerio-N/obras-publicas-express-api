7fae39ea58a76074bd28195ad203b4b692959b44
Rogerio N <rogenakayama@gmail.com>
2022-03-19 18:45:13 -0400

    Fix swagger importing config

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import express from "express";
 import client from "./src/config/database.js";
 import bodyParser from 'body-parser';
 import { userRouter } from "./src/routes/users.js";
+import { readFile } from 'fs/promises';
 import { themesRouter } from "./src/routes/themes.js";
 import { complaintRouter } from "./src/routes/complaint.js";
 
 import swaggerUi from 'swagger-ui-express'
-import swaggerDocs from './src/swagger.json'
 
 const app = express();
+
+const swaggerDocs = JSON.parse(
+    await readFile(
+        new URL("./src/swagger.json",import.meta.url)
+    )
+)
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -20,4 +27,6 @@ app.use("/complaints", complaintRouter);
 
 const port = 3000 || process.env.PORT;
 
-app.listen(port)
+app.listen(port, () => {
+    console.log("listening to port "+port)
+})

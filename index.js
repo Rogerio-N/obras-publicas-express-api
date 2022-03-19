0d1741572117a800f06bc50ca50d49660cd3c859
Rogerio N <rogenakayama@gmail.com>
2022-03-19 15:10:29 -0400

    Create swagger documentation

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ import { userRouter } from "./src/routes/users.js";
 import { themesRouter } from "./src/routes/themes.js";
 import { complaintRouter } from "./src/routes/complaint.js";
 
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './src/swagger.json'
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use("/users", userRouter);
 app.use("/themes", themesRouter);
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,10 +2,10 @@ import jwt from 'jsonwebtoken'
 
 export default function verifyJWT(req, res, next) {
     const token = req.headers['x-access-token'];
-    if (!token) return res.status(401).json({ auth: false, message: 'Token nao foi enviado.' });
+    if (!token) return res.status(400).json({ auth: false, message: 'Token nao foi enviado.' });
     
     jwt.verify(token, process.env.SECRET, function(err, decoded) {
-      if (err) return res.status(401).json({ auth: false, message: 'Falha ao autentica o token.' });
+      if (err) return res.status(401).json({ auth: false, messagem: 'Falha ao autentica o token.' });
       req.userId = decoded.userId;
       req.userEmail = decoded.userEmail
       req.userRole = decoded.userRole

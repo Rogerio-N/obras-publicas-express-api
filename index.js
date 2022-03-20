bbe5a2cd109b597f337b9e8d6ded2a6cd0702a54
Rogerio N <rogenakayama@gmail.com>
2022-03-20 00:03:12 -0400

    FIX port listener

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/users", userRouter);
 app.use("/themes", themesRouter);
 app.use("/complaints", complaintRouter);
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("listening to port "+port)
